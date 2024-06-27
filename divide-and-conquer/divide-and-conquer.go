19205ffdf5481018b2a1c5ef0ce3d34136c415ec
Furkan Topaloğlu <furkan.topaloglu@cimri.com>
2024-06-27 09:36:50 +0300

    add recursive count and findMax

--- divide-and-conquer/divide-and-conquer.go
+++ divide-and-conquer/divide-and-conquer.go
@@ -1,8 +1,30 @@
 package main
 
 func sum(nums []int) int {
-	if len(nums) == 1 {
-		return nums[0]
+	if len(nums) == 0 {
+		return 0
 	}
 	return nums[0] + sum(nums[1:])
+}
+
+func count(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	return 1 + count(nums[1:])
+
+}
+
+func findMax(nums []int, n int) int {
+	if n == 1 {
+		return nums[0]
+	}
+	max := findMax(nums, n-1)
+
+	if nums[n-1] > max {
+		return nums[n-1]
+	}
+
+	return max
 }
